c9250b5772ced802ad3473bb1b47ace04fc22077
Ryan-O-Connor <74995807+Ryan-O-Connor@users.noreply.github.com>
2022-04-23 20:58:18 -0500

    Update ecc.py

--- src/ecc.py
+++ src/ecc.py
@@ -1,3 +1,4 @@
+from cgi import test
 import unittest
 
 from primitives import *
@@ -67,6 +68,9 @@ class secp256k1FieldElement(FieldElement):
     def __repr__(self):
         return 'secp256k1FFE_({})'.format(self.num)
 
+    def sqrt(self):
+        return self**((secp256k1_PRIME + 1) // 4)
+
 
 class ECPoint:
 
@@ -83,7 +87,7 @@ class ECPoint:
         if self.x is None and self.y is None:
             pt = "Point(infinity)"
         else:
-            pt = "Point ({}, {})".format(self.x, self.y)
+            pt = "Point({:x}, {:x})".format(self.x.num, self.y.num)
         return "{} on elliptic curve a={}, b={}".format(pt, self.a, self.b)
 
     def _assert_same_curve(self, other):
@@ -151,18 +155,69 @@ class secp256k1Point(ECPoint):
         if self.x is None and self.y is None:
             pt = "Point(infinity)"
         else:
-            pt = "Point ({}, {})".format(self.x, self.y)
-        return "{} on secp256k1".format(pt, self.a, self.b)
+            pt = "Point({:x}, {:x})".format(self.x.num, self.y.num)
+        return "{} on secp256k1".format(pt)
 
     def __rmul__(self, coefficient):
         return super().__rmul__(coefficient % secp256k1_N)
 
     def verify(self, z, sig):
+        '''Verify signiture associated with public key'''
         s_inv = pow(sig.s, secp256k1_N - 2, secp256k1_N)
         u = z*s_inv % secp256k1_N
         v = sig.r * s_inv % secp256k1_N
         R = u*secp256k1_G + v*self
         return R.x.num == sig.r
+
+    def address(self, compressed=True, testnet=False):
+        '''Public key address'''
+        if testnet:
+            prefix = b'\x6f'
+        else:
+            prefix = b'\x00'
+        return encode_base58_checksum(prefix + self.hash160(compressed))
+
+    def hash160(self, compressed=True):
+        return hash160(self.sec(compressed))
+
+    def sec(self, compressed):
+        # Serialize public key in SEC format
+        if compressed:
+            if self.y.num % 2 == 0:
+                prefix = b'\x02'
+            else:
+                prefix = b'\x03'
+            sec_bin = prefix + self.x.num.to_bytes(32, 'big')
+        else:
+            sec_bin = b'\x04' + self.x.num.to_bytes(32, 'big') + self.y.num.to_bytes(32, 'big')
+        return sec_bin
+
+    @classmethod
+    def parse(cls, sec_bin):
+        # Parse binary SEC format into public key
+        prefix = sec_bin[0]
+        x = int.from_bytes(sec_bin[1:33], 'big')
+        if prefix == '\x04':
+            # Uncompressed SEC format
+            y = int.from_bytes(sec_bin[33:65], 'big')
+        else:
+            # Compressed SEC format
+            x = secp256k1FieldElement(x)
+            v = x**3 + secp256k1_B
+            w = v.sqrt()
+            if w.num % 2 == 0:
+                even_root = w
+                odd_root = secp256k1FieldElement(secp256k1_PRIME - w.num)
+            else:
+                even_root = secp256k1FieldElement(secp256k1_PRIME - w.num)
+                odd_root = w
+            if prefix == b'\x02':
+                y = even_root
+            elif prefix == b'\x03':
+                y = odd_root
+            else:
+                raise SyntaxError("Invalid SEC prefix {}".format(prefix))
+        return cls(x, y)
 
 
 # secp256k1 generator point
@@ -179,6 +234,26 @@ class Signiture:
     def __repr__(self):
         return "Signiture ({:x}, {:x})".format(self.r, self.s)
 
+    def der(self):
+        # Serialize signiture in DER format
+        result = b'\x02'
+        # Encode r
+        r_bin = self.r.to_bytes(32, 'big')
+        r_bin = r_bin.lstrip(b'\x00')
+        if r_bin[0] >= 0x80:
+            r_bin = b'\x00' + r_bin
+        result += len(r_bin).to_bytes(1, 'big') + r_bin
+        # Add marker
+        result += b'\x02'
+        # Encode s
+        s_bin = self.s.to_bytes(32, 'big')
+        s_bin = s_bin.lstrip(b'\x00')
+        if s_bin[0] >= 0x80:
+            s_bin = b'\x00' + s_bin
+        result += len(s_bin).to_bytes(1, 'big') + s_bin
+        # Encode length of result and prepend
+        return b'\x30' + len(result).to_bytes(1, 'big') + result
+
 
 class PrivateKey:
 
@@ -192,6 +267,7 @@ class PrivateKey:
         return self.key * secp256k1_G
 
     def sign(self, z):
+        '''Sign message hash'''
         k = 123456789
         k_inv = pow(k, secp256k1_N-2, secp256k1_N)
         R = (k*secp256k1_G)
@@ -200,6 +276,19 @@ class PrivateKey:
         if s > secp256k1_N / 2:
             s = secp256k1_N - s
         return Signiture(r, s)
+
+    def wif(self, compressed=True, testnet=False):
+        '''Encode private key in WIF format'''
+        if testnet:
+            prefix = b'\xef'
+        else:
+            prefix = b'\x80'
+        if compressed:
+            suffix = b'\x01'
+        else:
+            suffix = b''
+        return encode_base58_checksum(prefix + self.key.to_bytes(32, 'big') + suffix)
+        
 
 
 
@@ -323,14 +412,81 @@ class signingTests(unittest.TestCase):
 
     def testSign(self):
         private_key = PrivateKey(12345)
-        z = int.from_bytes(hash256(b'Programming Bitcoin!'), 'big')
+        z = int.from_bytes(hash256('Programming Bitcoin!'.encode()), 'big')
         sig = private_key.sign(z)
         public_key = private_key.public_key()
         self.assertTrue(public_key.verify(z, sig))
 
 
+class serializationTests(unittest.TestCase):
+
+    def testUncompressedSEC(self):
+        keys = (5000 , 2018**5, 0xdeadbeef12345)
+        secs = ("04ffe558e388852f0120e46af2d1b370f85854a8eb0841811ece0e3e03d282d57c315dc72890a4f10a1481c031b03b351b0dc79901ca18a00cf009dbdb157a1d10", 
+                "04027f3da1918455e03c46f659266a1bb5204e959db7364d2f473bdf8f0a13cc9dff87647fd023c13b4a4994f17691895806e1b40b57f4fd22581a4f46851f3b06", 
+                "04d90cd625ee87dd38656dd95cf79f65f60f7273b67d3096e68bd81e4f5342691f842efa762fd59961d0e99803c61edba8b3e3f7dc3a341836f97733aebf987121")
+        for key, sec in zip(keys, secs):
+            private_key = PrivateKey(key)
+            public_key = private_key.public_key()
+            sec_hex = public_key.sec(compressed=False).hex()
+            self.assertTrue(sec_hex == sec)
+
+    def testCompressedSEC(self):
+        keys = (5001 , 2019**5, 0xdeadbeef54321)
+        secs = ("0357a4f368868a8a6d572991e484e664810ff14c05c0fa023275251151fe0e53d1", 
+                "02933ec2d2b111b92737ec12f1c5d20f3233a0ad21cd8b36d0bca7a0cfa5cb8701", 
+                "0296be5b1292f6c856b3c5654e886fc13511462059089cdf9c479623bfcbe77690")
+        for key, sec in zip(keys, secs):
+            private_key = PrivateKey(key)
+            public_key = private_key.public_key()
+            sec_hex = public_key.sec(compressed=True).hex()
+            self.assertTrue(sec_hex == sec)
+
+    def testDER(self):
+        r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
+        s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
+        der = "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec"
+        sig = Signiture(r, s)
+        der_hex = sig.der().hex()
+        self.assertTrue(der_hex == der)
+
+    def testBase58(self):
+        hex_codes = ("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d", 
+                        "c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6",
+                        "eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c")
+        base58_codes = ("9MA8fRQrT4u8Zj8ZRd6MAiiyaxb2Y1CMpvVkHQu5hVM6", 
+                        "EQJsjkd6JaGwxrjEhfeqPenqHwrBmPQZjJGNSCHBkcF7",
+                        "4fE3H2E6XMp4SsxtwinF7w9a34ooUrwWe4WsW1458Pd")
+        for hex_code, base58_code in zip(hex_codes, base58_codes):
+            hex_base58 = encode_base58(bytes.fromhex(hex_code))
+            self.assertTrue(hex_base58 == base58_code)
+
+    def testAddresses(self):
+        keys = (5002 , 2020**5, 0x12345deadbeef)
+        addresses = ("mmTPbXQFxboEtNRkwfh6K51jvdtHLxGeMA",
+                    "mopVkxp8UhXqRYbCYJsbeE1h1fiF64jcoH",
+                    "1F1Pn2y6pDb68E5nYJJeba4TLg2U7B6KF1")
+        compressions = (False, True, True)
+        networks = (True, True, False)
+        for key, correct_address, compression, network in zip(keys, addresses, compressions, networks):
+            private_key = PrivateKey(key)
+            public_key = private_key.public_key()
+            address = public_key.address(compression, network)
+            self.assertTrue(address == correct_address)
+
+    def testWif(self):
+        keys = (5003 , 2021**5, 0x54321deadbeef)
+        wifs = ("cMahea7zqjxrtgAbB7LSGbcQUr1uX1ojuat9jZodMN8rFTv2sfUK",
+                "91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjpWAxgzczjbCwxic",
+                "KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgiuQJv1h8Ytr2S53a")
+        compressions = (True, False, True)
+        networks = (True, True, False)
+        for key, correct_wif, compression, network in zip(keys, wifs, compressions, networks):
+            private_key = PrivateKey(key)
+            wif = private_key.wif(compression, network)
+            self.assertTrue(wif == correct_wif)
+
+        
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-    
-    
-    
